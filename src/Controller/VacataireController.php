068eec6707a2fa4c2ca92d0362da0fb4e026a56c
healtyromain <romainlecrotgaulier@gmail.com>
2026-06-25 14:24:14 +0200

    Liste des vacataires

---
+++ src/Controller/VacataireController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\VacataireRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class VacataireController extends AbstractController
+{
+    #[Route('/vacataire', name: 'app_vacataire')]
+    public function index(
+        VacataireRepository $repository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $vacataires = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/vacataire/index.html.twig', [
+            'vacataires' => $vacataires,
+        ]);
+    }
+}
